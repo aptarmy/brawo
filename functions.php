c93ac9778ced4983322f5741c6a0af9bbaff8468
aptarmy <apt.enjoy@gmail.com>
2016-01-15 21:55:29 +0700

    remove skip-link-focus functionality.

--- functions.php
+++ functions.php
@@ -177,8 +177,6 @@ function _s_scripts() {
 	//wp_enqueue_style( '_s-icon-font', get_template_directory_uri() . '/libs/font-awesome/css/font-awesome.min.css' );
 	wp_enqueue_style( '_s-genericons', get_template_directory_uri() . '/libs/genericons/genericons.css' );
 
-	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
